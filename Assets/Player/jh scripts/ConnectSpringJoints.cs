59f02acb4f2e003d2fdcc5734e53504265405827
jh213232 <87266095+jh213232@users.noreply.github.com>
2025-09-16 17:22:46 +0800

    slime shape fix
    fixed

---
+++ Assets/Player/jh scripts/ConnectSpringJoints.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectSpringJoints : MonoBehaviour
+{
+    List<GameObject> SpringPoints = new List<GameObject>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DetectAndAttachSpringPoints()
+    {
+        foreach (Transform child in transform)
+        {
+
+        }
+    }
+
+}
