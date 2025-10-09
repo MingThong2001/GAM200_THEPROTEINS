a7114a73d24cbe6643300820cccca5caa07c42f2
MingThong2001 <2014ooimingthong@gmail.com>
2025-10-09 15:19:43 +0800

    Moving Platform BugFix
    Player will not be sliding off the moving platforms and now able to jump

--- Assets/Obstacles/MovingPlatforms.cs
+++ Assets/Obstacles/MovingPlatforms.cs
@@ -2,24 +2,41 @@ using UnityEngine;
 
 public class MovingPlatforms : MonoBehaviour
 {
+    //Platform Points.
     public Transform pointA;
     public Transform pointB;
-    public float moveSpeed = 2f;
-    private Vector3 nextPosition;
 
+    //Platform Settings.
+    public float moveSpeed = 2f;
+    private Vector2 nextPosition;
+    private Rigidbody2D rb;
     public void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
 
         nextPosition = pointB.position;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
 
-        if (transform.position == nextPosition)
-        { 
-            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;   
+        //if (transform.position == nextPosition)
+        //{ 
+        //    nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;   
+        //}
+
+        Vector2 newPos = Vector2.MoveTowards(rb.position, nextPosition, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        //Swap target when reaching destination
+        if (Vector2.Distance(rb.position, nextPosition) <= 0.01f)
+        {
+            nextPosition = (nextPosition == (Vector2)pointA.position)
+              ? pointB.position
+              : pointA.position;
         }
     }
 
