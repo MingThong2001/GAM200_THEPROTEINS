2321a7d3c3c4ebda7056d73b4b0e385f113db2ee
MingThong2001 <2014ooimingthong@gmail.com>
2025-10-13 11:05:32 +0800

    Sliders logic for audio manager

--- Assets/Sound Design/AudioManager.cs
+++ Assets/Sound Design/AudioManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource BGM;
     [SerializeField] AudioSource SFX;
+
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
 
     //References
 
@@ -27,13 +33,50 @@ public class AudioManager : MonoBehaviour
 
     public void Start()
     {
-        BGM.clip = background;
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        { 
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+
+            BGM.clip = background;
         BGM.Play();
     }
 
     public void PlaySFX(AudioClip clip)
     {
         SFX.PlayOneShot(clip);
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+
+    }
+
+
+    //Store Music Setting.
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
     }
 }
 
