11af793be5eb58a1d6df1b43b9ce248081cde69f
MingThong2001 <2014ooimingthong@gmail.com>
2025-10-31 00:46:03 +0800

    Update Game manager
    THERES CUT SCENE PANEL IN GAMEPLAY SCENEE. SO
    
    MAIN MENU > CUTSCENE > OBJECTIVE PANEL >GAMEPLAY
    
    IF RESTART:
     OBJECTIVE PANEL >GAMEPLAY
    
    IF BACK TO MAIN MENU THEN PLAY
    MAIN MENU > OBJECTIVE PANEL >GAMEPLAY

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,6 +10,7 @@ using UnityEngine.UI;
 public enum GameState
 {
     Play,
+    Cutscene,
     Paused,
     GameOver,
     Restart,
@@ -17,6 +18,13 @@ public enum GameState
     showobjective
  
 }
+
+//This is a static class to store persistent flags between scenes.
+public static class GameFlags
+{
+    public static bool hasPlayedCutscene = false; //Track if this cutscene has been played.
+}
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
@@ -27,12 +35,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject playMenu;
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject gameOverMenu;
-    //[SerializeField] private GameObject restartMenu;
     [SerializeField] private GameObject victoryMenu;
     [SerializeField] private GameObject objectivePanel;
     [SerializeField] private GameObject SettingsPanel;
+    [SerializeField] private GameObject cutscenePanel;
+
     //[SerializeField] private GameObject[] inGameText;
-    private GameObject previousMenu;
+    private GameObject previousMenu; //This store the menu that open settings either from pause menu or main menu.
 
     #endregion
 
@@ -50,6 +59,7 @@ public class GameManager : MonoBehaviour
     public bool gamePaused = false;
     public bool gameOver = false;
     private static bool isRestarting = false;
+    //private static bool hasCutscene = false;
 
 
     //CheckPoint
@@ -68,24 +78,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Drone drone;
     [SerializeField] EnemyPatrolDrone enemyPatrolDrone;
 
-    [SerializeField] private HelperWelper helperWelper; // prefab in inspector
-    [SerializeField] private EnemyPatrol helperWelperEnemyPatrol; // the EnemyPatrol script
-    [SerializeField] private Transform helperWelperSpawnPos; // Add this to inspector
-
-
+    [SerializeField] private HelperWelper helperWelper; 
+    [SerializeField] private EnemyPatrol helperWelperEnemyPatrol; 
+    [SerializeField] private Transform helperWelperSpawnPos; 
 
     [SerializeField] private FailedSubjects failedSubjects;
     [SerializeField] ChargePatrol chargePatrol;
 
 
    [Header("Obstacles SETTINGS")]
-    //Reset Door
     public Door[] doors;
     public DoorButton[] doorsButton;
 
     [Header("References")]
-
-    //References
     public PlayerShoot playershoot;
     public PlayerMovement playermovement;
     public SceneController sceneController;
@@ -108,26 +113,31 @@ public class GameManager : MonoBehaviour
         Debug.Log("GameManager Awake on object: " + gameObject.name);
         if (playerPrefab == null) Debug.LogError("Player prefab NOT assigned!");
         if (playerspawnPos == null) Debug.LogError("Player spawn position NOT assigned!");
+
+        //Default State.
         currentState = GameState.Play;
 
+        //UI setup for normal start or restart.
         if (!isRestarting)
         {
-            // Normal start, show main menu
+            //If start normally, show main menu.
             playMenu.SetActive(true);
             pauseMenu.SetActive(false);
             victoryMenu.SetActive(false);
             objectivePanel.SetActive(false);
+            if (cutscenePanel != null) cutscenePanel.SetActive(false);
 
             //foreach (GameObject text in inGameText)
             //    text.SetActive(true);
         }
         else
         {
-            // Restarting gameplay, hide main menu
+            //If restart, hide main menu.
             playMenu.SetActive(false);
             pauseMenu.SetActive(false);
             victoryMenu.SetActive(false);
             objectivePanel.SetActive(false);
+            if (cutscenePanel != null) cutscenePanel.SetActive(false);
 
             //foreach (GameObject text in inGameText)
             //    text.SetActive(false);
@@ -136,7 +146,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    //Get player controller.
     public void Start()
     {
         if (player == null)
@@ -145,7 +154,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            // If the player already exists in the scene (from scene setup), assign references
+            //If the player already exists in the scene (from scene setup), assign references.
             playermovement = player.GetComponent<PlayerMovement>();
             playershoot = player.GetComponent<PlayerShoot>();
             playerStats = player.GetComponent<PlayerStats>();
@@ -165,7 +174,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
-
+        //Disable player controls when any UI is active.
         bool uiActive = (playMenu.activeSelf || pauseMenu.activeSelf || victoryMenu.activeSelf || objectivePanel.activeSelf) || gamePaused;
         if (playermovement != null)
             playermovement.enabled = !uiActive;
@@ -188,20 +197,56 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("[GameManager] StartGame() called");
+        #region old code
+        //if (cutscenePanel != null)
+        //{
+        //    cutscenePanel.SetActive(true);
+        //    objectivePanel.SetActive(false);
+        //    currentState = GameState.Paused;
+        //    gamePaused = true;
+        //    Time.timeScale = 0f;
+        //}
+        //else
+        //{
+        //    //Scenes without cutscene go straight to objective
+        //    objectivePanel.SetActive(true);
+        //    currentState = GameState.showobjective;
+        //    gamePaused = true;
+        //    Time.timeScale = 0f;
+        //}
 
+        //gameOver = false;
+        //SpawnPlayer();
+        #endregion
+
+        //Hide menu.
         playMenu.SetActive(false);
-        objectivePanel.SetActive(true);
-        currentState = GameState.showobjective;
 
-        gamePaused = true;
+        // Check if the scene has a cutscene panel and the cutscene has not been played yet.
+        if (cutscenePanel != null && !GameFlags.hasPlayedCutscene)
+        {
+            //If first time showing, showcut scene.
+            cutscenePanel.SetActive(true);
+            objectivePanel.SetActive(false);
+            currentState = GameState.Cutscene;
+            gamePaused = true;
+            Time.timeScale = 0f;
+            GameFlags.hasPlayedCutscene = true; //We then marked it as flagged.
+        }
+        else
+        {
+            //If restart or scene without any cutscene, start from objective panel.
+            objectivePanel.SetActive(true);
+            currentState = GameState.showobjective;
+            gamePaused = true;
+            Time.timeScale = 0f;
+        }
+
         gameOver = false;
-
-        Time.timeScale = 0f;
         SpawnPlayer();
-
     }
 
-
+    //To transit it to play state.
     public void afterObjective()
     {
         objectivePanel.SetActive(false);
@@ -210,6 +255,23 @@ public class GameManager : MonoBehaviour
         currentState = GameState.Play;
 
     }
+
+    //When cutscene is complete.
+    public void OnCutsceneComplete()
+    {
+        //Hide cutscene panel.
+        if (cutscenePanel != null)
+            cutscenePanel.SetActive(false);
+
+        //Then show obejctive panel
+        objectivePanel.SetActive(true);
+
+        currentState = GameState.showobjective;
+        gamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+
     #endregion
 
 
@@ -225,7 +287,7 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 0f;
             pauseMenu.SetActive(true);
 
-            // Disable movement and shooting when paused
+            //We disable any interaction here.
             if (playermovement != null)
                 playermovement.enabled = false;
 
@@ -241,7 +303,7 @@ public class GameManager : MonoBehaviour
             pauseMenu.SetActive(false);
             currentState = GameState.Play;
 
-            // Re-enable movement and shooting when unpaused
+            // And reEnable it afterward.
             if (playermovement != null)
                 playermovement.enabled = true;
 
@@ -253,6 +315,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
+
+    //Resume game from pause menu.
     public void resumeGame()
     {
 
@@ -298,7 +362,8 @@ public class GameManager : MonoBehaviour
             Debug.LogError("[GameManager] playerspawnPos is NULL!");
             return;
         }
-        // Reset stale reference
+
+        //Reset stale reference. (Safety-net).
         if (player != null && !player.gameObject.activeInHierarchy)
         {
             player = null;
@@ -307,22 +372,22 @@ public class GameManager : MonoBehaviour
         if (player == null)
         {
 
-            // Instantiate new player prefab at spawn position
+            //Instantiate new player prefab at spawn position.
             player = Instantiate(playerPrefab, playerspawnPos.position, Quaternion.identity);
             SetPlayer(player.transform);
             player.name = "Player";
             Debug.Log("[GameManager] Player spawned at: " + player.transform.position);
 
-            // Assign references
+            //Assign key references.
             playermovement = player.GetComponentInParent<PlayerMovement>();
             playershoot = player.GetComponentInParent<PlayerShoot>();
             playerStats = player.GetComponentInParent<PlayerStats>();
             playerStats.healthBarUI = FindObjectOfType<HealthBarUI>();
             puddleController = player.GetComponentInParent<PuddleController>();
 
-            // Initialize the player
+            //Initialize the player.
             playerStats.InitializePlayer();
-            // Reassign camera reference
+
             // Assign camera
             CameraController cam = Camera.main.GetComponent<CameraController>();
             if (cam != null)
@@ -332,6 +397,7 @@ public class GameManager : MonoBehaviour
         }
         else
         { 
+            //Reset position for existing player.
             player.transform.position = playerspawnPos.position;
             playerStats.InitializePlayer();
         }
@@ -376,7 +442,7 @@ public class GameManager : MonoBehaviour
 
         if (isVictory)
         {
-            // Win Condition
+           //Win condition.
             victoryMenu.SetActive(true);
             gameOverMenu.SetActive(false);
         }
@@ -384,13 +450,15 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("[GameManager] gameOverMenu active: " + gameOverMenu.activeSelf);
 
-            // Lose Condition
+         //Lose condition.
             victoryMenu.SetActive(false);
             gameOverMenu.SetActive(true);
         }
 
     }
 
+
+    //Reset vicotry menu and doors (not sure if this needed - tbc).
     public void resetVictory()
     {
         gameOver = false;
@@ -399,6 +467,8 @@ public class GameManager : MonoBehaviour
         ResetDoorandbutton();
     }
 
+
+    //Back to main menu (reload the current scene)
     public void BackToStartMenu()
     {
         Debug.Log("BackToStartMenu called");
@@ -409,24 +479,27 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+
+    //Restart the current level.
     public void restartGame()
     {
-        isRestarting = true;       // Tell GameManager we are restarting gameplay
+        isRestarting = true;       
         Time.timeScale = 1f;
         gamePaused = false;
         gameOver = false;
         currentState = GameState.Play;
 
-        // Get current scene
+        //Get current scene
         Scene currentScene = SceneManager.GetActiveScene();
 
-        // Reload it cleanly
+        //Reload it cleanly
         SceneManager.LoadScene(currentScene.name);
 
     }
 
     #region Reset
 
+    #region oldcode
     //private void DestroyAllEnemies()
     //{
     //    Debug.Log("[GameManager] Destroying all enemies...");
@@ -456,34 +529,6 @@ public class GameManager : MonoBehaviour
 
 
     //}
-    public void ResetDoorandbutton()
-    {
-        foreach (Door door in doors)
-        {
-            door.ResetDoor();
-            door.Playerinreange = false;
-
-        }
-
-        foreach (DoorButton button in doorsButton)
-        {
-            button.ResetButton();
-        }
-    }
-
-
-    public void resetGameState()
-    {
-        gamePaused = false;
-        gameOver = false;
-
-        playerStats.InitializePlayer();
-        spawnHelperWelper();
-
-
-    }
-    #endregion
-    #endregion
 
 
     //public void cleanupScene()
@@ -503,6 +548,43 @@ public class GameManager : MonoBehaviour
     //    activecheckPoints = null;
 
     //} 
+
+    #endregion 
+
+
+    //Reset doorbutton state.
+    public void ResetDoorandbutton()
+    {
+        foreach (Door door in doors)
+        {
+            door.ResetDoor();
+            door.Playerinreange = false;
+
+        }
+
+        foreach (DoorButton button in doorsButton)
+        {
+            button.ResetButton();
+        }
+    }
+
+
+
+
+    public void resetGameState()
+    {
+        gamePaused = false;
+        gameOver = false;
+
+        playerStats.InitializePlayer();
+        spawnHelperWelper();
+
+
+    }
+    #endregion
+    #endregion
+
+
     public void QuitGame()
     {
         ///Danielle said we need to have a confirmation panel for quiting.
@@ -547,6 +629,9 @@ public class GameManager : MonoBehaviour
         OpenSettings(pauseMenu);
     }
     #endregion
+
+
+
     #region CHECKPOINTS
 
     public void RegisterPlayer(GameManager playerObj)
